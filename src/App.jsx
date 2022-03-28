2ccfdf387f7c15e8eb967e732909ece79ba30072
Dandyndot29 <dandysetiawanptr@gmail.com>
2022-03-28 12:00:31 +0800

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Bookslist from './components/booksList';
 import Navbar from './components/Navbar'
 import Header from './components/Header';
 import Footer from './components/footer';
+import Login from './components/Login';
+
 
 
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <Navbar/>
       <Header/>
       <Bookslist/>
       <Footer/>
+     
      </div>
   );
 }
